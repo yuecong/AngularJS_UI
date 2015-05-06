f3941db37774d9483cb20ccfb09c5ae09af741ee
Cong Yue <cong_yue@alliedtelesis.com>
2015-05-05 17:45:02 -0700

    Initial commit

---
+++ EtherCacheUI/js/config.js
@@ -0,0 +1,92 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ * Copyright 2015 Webapplayers.com
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+    $urlRouterProvider.otherwise("/index/main");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "views/common/content.html",
+        })
+        .state('index.main', {
+            url: "/main",
+            templateUrl: "views/main.html",
+            data: { pageTitle: 'Ether Cache Management portal' }
+        })
+        .state('index.minor', {
+            url: "/minor",
+            templateUrl: "views/minor.html",
+            data: { pageTitle: 'Ether Cache Management portal' }
+        })
+        .state('index.workload_flot_chart', {
+            url: "/server_workload_chart",
+            templateUrl: "views/server_workload_chart.html",
+            data: { pageTitle: 'Server Workload' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            serie: true,
+                            name: 'angular-flot',
+                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('index.cache_performance_flot_chart', {
+            url: "/cache_performance_chart",
+            templateUrl: "views/cache_performance_chart.html",
+            data: { pageTitle: 'Cache Performance' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            serie: true,
+                            name: 'angular-flot',
+                            files: [ 'js/plugins/flot/jquery.flot.js', 'js/plugins/flot/jquery.flot.time.js', 'js/plugins/flot/jquery.flot.tooltip.min.js', 'js/plugins/flot/jquery.flot.spline.js', 'js/plugins/flot/jquery.flot.resize.js', 'js/plugins/flot/jquery.flot.pie.js', 'js/plugins/flot/curvedLines.js', 'js/plugins/flot/angular-flot.js', ]
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('index.access_statics', {
+            url: "/nggrid",
+            templateUrl: "views/access_statics_nggrid.html",
+            data: { pageTitle: 'Access Statics' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            name: 'ngGrid',
+                            files: ['js/plugins/nggrid/ng-grid-2.0.3.min.js']
+                        },
+                        {
+                            insertBefore: '#loadBefore',
+                            files: ['js/plugins/nggrid/ng-grid.css']
+                        }
+                    ]);
+                }
+            }
+        })
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
